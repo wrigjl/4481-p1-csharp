8e0ad4a42325cea0fd0a6b4eef8c5cea154d469f
Jason L. Wright <jAson@thought.net>
2025-11-26 04:12:35 +0000

    more tests

--- Source.Tests/PrimeService_IsPrimeShould.cs
+++ Source.Tests/PrimeService_IsPrimeShould.cs
@@ -1,17 +1,53 @@
-using Xunit;
+using System;
 using Prime.Services;
+using Xunit;
 
 namespace Prime.UnitTests.Services
 {
     public class PrimeService_IsPrimeShould
     {
-        [Fact]
-        public void IsPrime_InputIs1_ReturnFalse()
-        {
-            var primeService = new PrimeService();
-            bool result = primeService.IsPrime(1);
+        private readonly PrimeService _primeService;
 
-            Assert.False(result, "1 shouldn't be prime");
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        #region Sample_TestCode
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+        #endregion
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void IsPrime_PrimesLessThan10_ReturnTrue(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.True(result, $"{value} should be prime");
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void IsPrime_NonPrimesLessThan10_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
         }
     }
 }
--- source/PrimeService.cs
+++ source/PrimeService.cs
@@ -6,11 +6,19 @@ namespace Prime.Services
     {
         public bool IsPrime(int candidate)
         {
-            if (candidate == 1)
+            if (candidate < 2)
             {
                 return false;
             }
-            throw new NotImplementedException("Not fully implemented.");
+
+            for (var divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
+            {
+                if (candidate % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
